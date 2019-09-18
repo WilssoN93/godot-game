edec39c4cbd40d12ac3b2625f98de9cef3985347
WilssoN93 <38328228+WilssoN93@users.noreply.github.com>
2019-09-18 09:34:13 +0200

    changed some values to player

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -2,23 +2,24 @@ extends KinematicBody2D
 
 const UP = Vector2(0,-1)
 var motion = Vector2()
-const SPEED = 200
-const GRAVITY = 10
+const SPEED = 400
+const GRAVITY = 30
+const JUMP = -600
 
 func _physics_process(delta):
 	
 	motion.y += GRAVITY
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x =100
+		motion.x =SPEED
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -100
+		motion.x = -SPEED
 	else:
 		motion.x = 0
 	
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
-			motion.y = -550
+			motion.y = JUMP
 	
 	
 	motion = move_and_slide(motion, UP)
