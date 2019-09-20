1554ec45eb6fc778736cf6f09ef2b3e6acf1439e
WilssoN93 <wilssondaniel@gmail.com>
2019-09-20 22:23:18 +0200

    added some tileset

--- MoveableWall.gd
+++ MoveableWall.gd
@@ -1,18 +1,17 @@
-extends RigidBody2D
+extends KinematicBody2D
 const UP = Vector2(0,-1)
 var motion = Vector2(200,0)
 var SPEED = 200
 var direction = true
 
 func _physics_process(delta):
-	var bodies = get_colliding_bodies()
-	for body in bodies:
-		if body.name == "wall":
-			if direction:
-				motion.x = SPEED
-				direction =false
-				print("first if block")
-			else:
-				print ("second if block")
-				motion.x = -SPEED
-				direction = true
+	if is_on_wall():
+		if direction:
+			motion.x = SPEED
+			direction =false
+			print("first if block")
+		else:
+			print ("second if block")
+			motion.x = -SPEED
+			direction = true
+	motion = move_and_slide(motion,UP)
